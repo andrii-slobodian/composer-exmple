7aad66ca280f0e9464e5cdb926d42c3160444f3e
Andriy Slobodian <andrik7777785@gmail.com>
2019-05-10 12:37:29 +0300

    Small bug fixed

--- src/ParserInterface.php
+++ src/ParserInterface.php
@@ -3,26 +3,7 @@
 namespace excito\parser;
 
 
-class Parser implements ParserInterface {
+interface ParserInterface {
     
-    
-    public function process(string $url, string $tag): array {
-       
-        
-        $htmlPage = file_get_contents($url);
-        
-        
-        if($htmlPage === false){
-            return "Invalid URL";
-        }
-        
-        preg_match_all("/<".$tag.".*?>(*.?)<\/".$tag.">/s", $htmlPage, $strings);
-        
-        if (empty($strings[1])){
-            return "NO tag";
-        }
-        
-        return $strings[1];
-    }
+    public function process(string $url, string $tag):array;
 }
-
